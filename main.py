8a0954172d40229d7fb6fd4ebd99409309a05a18
きゅーしゅー <sjhgura@gmail.com>
2026-08-14 20:33:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import os
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'ログイン成功: {bot.user}')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+# 環境変数からトークンを取得して起動
+TOKEN = os.getenv('DISCORD_TOKEN')
+bot.run(TOKEN)
 
